598ecb6dde0fe3ababc5d44c7a10c67503027402
shiddik <shiddikurrahaman@gmail.com>
2023-12-17 13:57:27 +0600

    feature before after image done

--- js/custom.js
+++ js/custom.js
@@ -500,6 +500,25 @@
         });
 
         /*
+        Before After Js
+        ============================*/
+        $('.after-before-image-inner').beforeAfter({
+            movable: true,
+            clickMove: true,
+            position: 50,
+            separatorColor: '#fafafa',
+            bulletColor: '#fafafa',
+            onMoveStart: function(e) {
+                console.log(event.target);
+            },
+            onMoving: function() {
+                console.log(event.target);
+            },
+            onMoveEnd: function() {
+                console.log(event.target);
+            },
+        });
+        /*
         Scroll To Top Js
         ============================*/
         $(function () {
@@ -528,6 +547,8 @@
                 return false;
             });
         });
+
+
         /*
         Preeloader
         ============================*/
